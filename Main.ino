e35a3aa58a7b74e351378f41efc9ce8486398379
Jaden Plugowsky <124389316+Jaden-Plugowsky@users.noreply.github.com>
2023-09-26 11:22:17 -0400

    added code

---
+++ Main.ino
@@ -0,0 +1,19 @@
+// Created by: Jaden Plugowsky
+// Created on: Sept 2023
+//
+// This is a basic LED circuit.
+
+int pin5 = 5;
+
+void setup()
+{
+    pinMode(pin5, OUTPUT);
+}
+
+void loop()
+{
+    digitalWrite(pin5, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+    digitalWrite(pin5, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+}
